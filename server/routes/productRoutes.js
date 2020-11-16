c74dcb84fe67ba2a1e3737cb288f2c74a8f2f0a6
AGoldmintz <agoldmintz@gmail.com>
2020-11-16 15:47:58 -0600

    User routes, controllers, and middleware set up

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,30 +1,13 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
-
 const router = express.Router();
+import {
+	getProducts,
+	getProductById,
+} from '../controllers/productController.js';
 
 //Fetch all products
-router.get(
-	'/',
-	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		res.json(products);
-	}),
-);
+router.route('/').get(getProducts);
 
 //Fetch product by id
-router.get(
-	'/:id',
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-
-		if (product) {
-			res.json(product);
-		} else {
-			res.status(404).json({ message: 'product not found' });
-		}
-	}),
-);
-
+router.route('/:id').get(getProductById);
 export default router;
--- server/server.js
+++ server/server.js
@@ -2,15 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(express.json());
 
-//Products Route
+//Products Routes
 app.use('/api/products', productRoutes);
+
+//User Routes
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
