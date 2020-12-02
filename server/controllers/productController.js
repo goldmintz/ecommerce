26c5dbb43883b6faceb6eb2426d26909a91d3ab8
AGoldmintz <agoldmintz@gmail.com>
2020-12-02 10:58:00 -0600

    Reviews controller and product model updated

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -16,6 +16,42 @@ const getProductById = asyncHandler(async (req, res) => {
 	}
 });
 
+const createReview = asyncHandler(async (req, res) => {
+	const { rating, comment } = req.body;
+
+	const product = await Product.findById(req.params.id);
+	if (product) {
+		const existingReviews = product.reviews.find(
+			(review) => review.user.toString() === req.user._id.toString(),
+		);
+
+		if (existingReviews) {
+			res.status(400);
+			throw new Error('Product already reviewed by user');
+		}
+
+		const review = {
+			name: req.user.name,
+			rating: Number(rating),
+			comment,
+			user: req.user._id,
+		};
+
+		product.reviews.push(review);
+		product.numReviews = product.reviews.length;
+		product.rating =
+			product.reviews.reduce((acc, el) => el.rating + acc, 0) /
+			products.reviews.length;
+
+		await product.save();
+		res.status(201).json({ message: 'Review successfully added' });
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
+	}
+});
+
+//ADMIN ONLY PROTECTED ROUTES
 //admin only to delete
 const deleteProduct = asyncHandler(async (req, res) => {
 	const product = await Product.findById(req.params.id);
@@ -75,9 +111,11 @@ const updateProduct = asyncHandler(async (req, res) => {
 		throw new Error('Product not found');
 	}
 });
+
 export {
 	getProducts,
 	getProductById,
+	createReview,
 	deleteProduct,
 	createProduct,
 	updateProduct,
