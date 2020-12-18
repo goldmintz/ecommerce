ca40108d76b0396654428514d4410689ef7a513b
AGoldmintz <agoldmintz@gmail.com>
2020-12-18 13:38:04 -0600

    Hide Seasonal Prods grid on smaller screens

--- client/src/components/layout/SeasonalProds.js
+++ client/src/components/layout/SeasonalProds.js
@@ -4,22 +4,35 @@ import Product from '../features/Product';
 
 const SeasonalProds = () => {
 	return (
-		<Container fluid>
+		<Container fluid className='seasonal-prods'>
 			<h2>Seasonal Products</h2>
 			<hr></hr>
-			<Row fluid>
-				<Col md={6} lg={6}>
-					<Image src='/images/monstera.jpg' fluid />
-				</Col>
-				<Col md={3}>
-					<Image src='/images/monstera.jpg' fluid />
-					<Image src='/images/monstera.jpg' fluid />
-				</Col>
-				<Col md={3}>
-					<Image src='/images/monstera.jpg' fluid />
-					<Image src='/images/monstera.jpg' fluid />
-				</Col>
-			</Row>
+			<Container fluid >
+				<Row >
+					<Col md={6}>
+						<div className='h-100'>
+							<Image src='/images/monstera.jpg' fluid></Image>
+						</div>
+					</Col>
+
+					<Col md={3}>
+						<div style={{ height: '50%' }}>
+							<Image src='/images/monstera.jpg' fluid></Image>
+						</div>
+						<div style={{ height: '50%' }}>
+							<Image src='/images/monstera.jpg' fluid></Image>
+						</div>
+					</Col>
+					<Col md={3}>
+						<div style={{ height: '50%' }}>
+							<Image src='/images/monstera.jpg' fluid></Image>
+						</div>
+						<div style={{ height: '50%' }}>
+							<Image src='/images/monstera.jpg' fluid></Image>
+						</div>
+					</Col>
+				</Row>
+			</Container>
 		</Container>
 	);
 };
