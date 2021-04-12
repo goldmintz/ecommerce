2f1dea0bf63537d6bb83ab8e45e1f838d609f49f
AGoldmintz <agoldmintz@gmail.com>
2021-04-12 14:15:25 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
-	5000,
+	PORT,
 	console.log(
 		`Server running in ${process.env.NODE_ENV} on good ol' Port${PORT}.`,
 	),
